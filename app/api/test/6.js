d8507434e9deee9e8863ac4323e5801be9baa422
hbbaly <1422699902@qq.com>
2019-07-10 22:47:59 +0800

    ADD：查找某个对象上所有属性名及方法名

--- app/api/test/6.js
+++ app/api/test/6.js
@@ -2,8 +2,14 @@ const Router = require('koa-router')
 const router = new Router()
 const {PositiveIntegerValidator} = require('../../../lib/validator.js')
 router.get('/error/:id',async (ctx, next) => {
+  const path = ctx.params, 
+        query = ctx.request.query, 
+        header = ctx.request.header, 
+        body = ctx.request.body
     const error = await new PositiveIntegerValidator().validate(ctx)
-    console.log(error, '123456')
+    // 。get(path， parsed)  // 获取参数，path参数路径，parsed是否进行类型转换
+    const id = error.get('path.id')
+    console.log(id,'di')
     ctx.body='validator'
 })
 module.exports = router
